0dbe42e33e59c8c960f00aac94c687d46ce2764b
Eli Ribble <eli@theribbles.org>
2024-11-23 13:29:40 -0700

    gofmt is required now

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 	r.StaticFile("/", "./static/index.html")
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    r.GET("/hello", getHello)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/hello", getHello)
 	r.Static("/static", "./static")
-    _ = r.Run()
+	_ = r.Run()
 }
 
 func getHello(c *gin.Context) {
-    c.String(http.StatusOK, "Hello World!")
+	c.String(http.StatusOK, "Hello World!")
 }
