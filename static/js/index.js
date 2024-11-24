ef14e9468155929243f34fdd3d21fcff5206d36f
Eli Ribble <eli@theribbles.org>
2024-11-24 16:15:04 -0700

    Show the registration form when login isn't working.

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	console.log("Loaded");
-	var form = document.getElementById("login");
-	form.addEventListener("submit", onLoginSubmit, true);
+	const login_form = document.getElementById("login");
+	login_form.addEventListener("submit", onLoginNext, true);
+	const register_form = document.getElementById("register");
+	register_form.addEventListener("submit", onRegisterNext, true);
 });
 
-async function onLoginSubmit(e) {
+function showRegisterForm() {
+	console.log("Showing registration form");
+	document.getElementById("login").style.display = "none";
+	document.getElementById("register").style.display = "block";
+}
+
+async function onLoginNext(e) {
 	e.preventDefault();
-	var username_element = document.getElementById("username");
+	var username_element = document.querySelector("#login input[name='username']");
 	const username = username_element.value;
 	const url = "/login/begin?username=" + encodeURIComponent(username);
 	const response = await fetch(url);
+	if (response.status == 400) {
+		showRegisterForm();
+	}
 	const json = await response.json()
 	console.log(json);
 }
 
+async function onRegisterNext(e) {
+	e.preventDefault();
+	var username_element = document.querySelector("#register .username");
+	console.log("register username", username_element);
+}
