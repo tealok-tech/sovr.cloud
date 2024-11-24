cc8bba788d8aced19ec636d9ef99664473c9b82e
Eli Ribble <eli@theribbles.org>
2024-11-24 15:39:49 -0700

    Don't actually navigate when we hit the "next" button.
    We want to do asynchronous work.

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,11 @@
-console.log("Hey there.");
+document.addEventListener("DOMContentLoaded", function(event) {
+	console.log("Loaded");
+	var form = document.getElementById("login");
+	form.addEventListener("submit", onLoginSubmit, true);
+});
+
+function onLoginSubmit(e) {
+	e.preventDefault();
+	console.log("hey");
+}
+
