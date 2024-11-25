19e7be2733bf082efd721a7a2e070c9d192e752a
Eli Ribble <eli@theribbles.org>
2024-11-25 11:26:53 -0700

    Save session data in a local file.
    It's like a database, but simpler.

--- datastore.go
+++ datastore.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/gob"
 	"errors"
 	"github.com/go-webauthn/webauthn/webauthn"
+	"os"
 )
 
 type Datastore struct {
+	sessions []webauthn.SessionData
 }
 
 func (d *Datastore) GetUser(username string) (User, error) {
@@ -15,6 +18,15 @@ func (d *Datastore) GetSession() webauthn.SessionData {
 	return webauthn.SessionData{}
 }
 func (d *Datastore) SaveSession(u *webauthn.SessionData) {
+	d.sessions = append(d.sessions, *u)
+
+	f, err := os.Open("datastore.glob")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	enc := gob.NewEncoder(f)
+	err = enc.Encode(d)
 }
 func (d *Datastore) SaveUser(u User) {
 }
