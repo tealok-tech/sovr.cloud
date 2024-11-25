323d3b935cc37e1e4d8706c065b6532daa5921d3
Eli Ribble <eli@theribbles.org>
2024-11-25 08:11:38 -0700

    Show errors when necessary APIs are unavailable.

--- static/js/index.js
+++ static/js/index.js
@@ -3,12 +3,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	document.getElementById("js_loading").style.display = "none";
 	if (!window.PublicKeyCredential) {
 		document.getElementById("no_webauthn").style.display = "block";
-		return;
 	}
+	let p1 = new Promise((resolve, reject) => {
+		if (!PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable) {
+			document.getElementById("no_platform_auth_api").style.display = "block";
+			reject("IsUserVerifyingPlatformAuthenticatorAvailable is not available");
+		} else {
+			PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable().then((available) => {
+				console.log("isUserVerifyingPlatformAuthenticatorAvailable:", available);
+				if (!available) {
+					document.getElementById("no_platform_auth").style.display = "block";
+				}
+				resolve(available);
+			});
+		}
+	});
+	let p2 = new Promise((resolve, reject) => {
+		if (!PublicKeyCredential.isConditionalMediationAvailable) {
+			document.getElementById("no_conditional_mediation_api").style.display = "block";
+			reject("IsConditionalMediationAvailable is not available");
+		} else {
+			PublicKeyCredential.isConditionalMediationAvailable().then((available) => {
+				console.log("isConditionalMediationAvailable:", available);
+				if (!available) {
+					document.getElementById("no_conditional_mediation").style.display = "block";
+				}
+				resolve(available);
+			});
+		}
+	});
 	const login_form = document.getElementById("login");
 	login_form.addEventListener("submit", onLoginNext, true);
 	const register_form = document.getElementById("register");
 	register_form.addEventListener("submit", onRegisterNext, true);
+	Promise.all([p1, p2]).then((values) => {
+		if(values[0] && values[1]) {
+			login_form.style.display = "block";
+		}
+	})
 });
 
 function showRegisterForm() {
