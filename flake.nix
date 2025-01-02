6e16185c7c826c0dee24b679ff3ad35b2c034191
Eli Ribble <eli@theribbles.org>
2025-01-02 07:43:05 -0700

    Add necessary systemd configuration
    This program is configured through environment variables. This starts
    the process of exposing those configuration values through nix
    configuration.
    
    Hat tip to https://codeberg.org/Lenny/float and
    https://blog.lenny.ninja/posts/2023-04-16-packaging-nix-services-2.html
    for both a blog post and a working example to work from.

--- flake.nix
+++ flake.nix
@@ -21,22 +21,54 @@
     {
       nixosModules.default = { config, pkgs, lib, ... }: {
         options = {
-	  services.sovr-server.enable = lib.mkEnableOption "sovr-server";
-	};
+          services.sovr-server = {
+            enable = lib.mkEnableOption "sovr-server service";
+            port = lib.mkOption {
+              type = lib.types.port;
+              default = 8080;
+              description = "the port to serve requests on";
+            };
+            sessionSecret = lib.mkOption {
+              type = lib.types.str;
+              default = "please-change-me";
+              description = "the secret used for generating session cookies";
+            };
+          };
+        };
 
-	config = lib.mkIf config.services.sovr-server.enable {
-	  systemd.services.sovr-server = {
-	    serviceConfig.ExecStart = "${self.packages.${pkgs.system}.default}/bin/main";
-	  };
-	};
+        config = lib.mkIf config.services.sovr-server.enable {
+          systemd.services.sovr-server = {
+            after = [ "network.target" ];
+            enable = true;
+            environment = {
+              GIN_MODE = "release";
+              PORT = "${toString config.services.sovr-server.port}";
+              RELYING_PARTY_DISPLAY_NAME = "Sovr Cloud";
+              RELYING_PARTY_ID = "www.sovr.cloud";
+              RELYING_PARTY_ORIGINS = "https://www.sovr.cloud";
+              SESSION_SECRET = "${toString config.services.sovr-server.sessionSecret}";
+              TEMPLATES_DIR = "/opt/sovr.cloud/templates";
+              TRUSTED_PROXIES = "127.0.0.1,::1";
+            };
+            serviceConfig = {
+              ExecStart = "${self.packages.${pkgs.system}.default}/bin/sovr.cloud";
+              Group = "deploy";
+              Restart = "on-failure";
+              Type = "simple";
+              User = "deploy";
+              WorkingDirectory = "/opt/sovr.cloud";
+            };
+            wantedBy = [ "multi-user.target" ];
+          };
+        };
       };
       packages = forAllSystems ({ pkgs, system }: {
         default = self.packages.${system}.sovr-server;
         sovr-server = pkgs.buildGoModule rec {
-	  overrideModAttrs = (oldAttrs: {
-	    preBuild = /* bash */ ''
-	      export GOPROXY=https://goproxy.io
-	    '';
+          overrideModAttrs = (oldAttrs: {
+            preBuild = /* bash */ ''
+              export GOPROXY=https://goproxy.io
+            '';
           });
           pname = "sovr-server";
           src = ./.;
